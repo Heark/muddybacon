aab561fdcdc2201c6a6c36d8d2e615d1ad70fe7c
Heark <hearkofsorcerey@gmail.com>
2015-01-15 21:39:01 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,24 @@
+var pigSprite;
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update });
+
+function preload() {
+	// Loading the soon to be drawn pig
+	 game.load.image('pig', 'pig.png');
+}
+function create() {
+	// Drawing the pig
+	 pigSprite = game.add.sprite(game.world.centerX, 0, 'pig');
+	 pigSprite.body.collideWorldBounds = true;
+}
+function update() {
+	if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+		pigSprite.velocity.x = -50;
+	} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+		pigSprite.velocity.x = 50;
+	}
+
+}
+function render() {
+
+}
